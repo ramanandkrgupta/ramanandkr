596fb003058de2d7e3445908f0d31b8ddc096e64
ramanandkrgupta <115969817+ramanandkrgupta@users.noreply.github.com>
2024-06-24 17:39:47 +0530

    Update email.js

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,47 +1,43 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementById('contactForm').addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevent the default form submission
+function sendMail() {
+    const form_name = document.getElementById("name").value;
+    const form_email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
 
-        const form_name = document.getElementById("name").value;
-        const form_email = document.getElementById("email").value;
-        const message = document.getElementById("message").value;
+    const params = {
+        form_name,
+        form_email,
+        message,
+    };
 
-        const params = {
-            form_name,
-            form_email,
-            message,
-        };
-
-        fetch("https://emailjs-sage.vercel.app/send-email", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(params)
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Network response was not ok: " + response.statusText);
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log("Email sent:", data);
-            Swal.fire({
-                title: 'Success!',
-                text: 'Email sent successfully!',
-                icon: 'success',
-                confirmButtonText: 'OK'
-            });
-        })
-        .catch(error => {
-            console.error("Error sending email:", error);
-            Swal.fire({
-                title: 'Error!',
-                text: 'An error occurred while sending the email. Please try again later.',
-                icon: 'error',
-                confirmButtonText: 'OK'
-            });
+    fetch("https://emailjs-sage.vercel.app/send-email", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(params)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Network response was not ok: " + response.statusText);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log("Email sent:", data);
+        Swal.fire({
+            title: 'Success!',
+            text: 'Email sent successfully!',
+            icon: 'success',
+            confirmButtonText: 'OK'
+        });
+    })
+    .catch(error => {
+        console.error("Error sending email:", error);
+        Swal.fire({
+            title: 'Error!',
+            text: 'An error occurred while sending the email. Please try again later.',
+            icon: 'error',
+            confirmButtonText: 'OK'
         });
     });
-});
+}
